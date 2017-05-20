f4ff5651eae14374a848cddb8d36d3cb4c62cf6f
sciofu <sciofu@gmail.com>
2017-05-20 17:16:43 +0300

    Add files via upload

--- imdb.cpp
+++ imdb.cpp
@@ -8,7 +8,6 @@
 #include "include/imdb.h"
 #include "include/classes.h"
 
-
 IMDb::IMDb() {
     // initialize what you need here.
 }
@@ -43,6 +42,24 @@ void IMDb::add_user(std::string user_id, std::string name) {
 }
 
 void IMDb::add_actor(std::string actor_id, std::string name) {
+
+    actor new_actor(actor_id, name);
+    if(actors.empty()) {
+      actors.push_back(new_actor);
+      return;
+    }
+    int index = actors.size();
+    if(actor_id > actors[index -1].get_actor_id()) {
+      actors.push_back(new_actor);
+    } else {
+      for(auto it = actors.begin(); it != actors.end(); ++it) {
+        if (it->get_actor_id() > actor_id) {
+          actors.insert(it, new_actor);
+          break;
+        }
+    }
+  }
+
 }
 
 void IMDb::add_rating(std::string user_id, std::string movie_id, int rating) {
